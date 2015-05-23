82b4543b12915d8023fbbaac8966574a99908c67
Mariya <mbeketova@rambler.ru>
2015-05-23 14:38:57 +0300

    Initial Commit

---
+++ ObjectiveC2_Lesson6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveC2_Lesson6
+//
+//  Created by Admin on 23.05.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
